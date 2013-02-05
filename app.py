baba2e347a731e430bf9ce51676eb2023d44eae3
Binh Le <binhl@vng.com.vn>
2013-02-05 23:59:29 +0700

    ported getrecenttracks api

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 import os
-from flask import Flask
+import json
+import time
+import functools
+from flask import Flask, request
+
+import lastfm
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+def app_api(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        before = time.time()
+        try:
+            data = fn(*args, **kwargs)
+        except:
+            result = {'error': 1}
+        else:
+            result = {'error': 0, 'data': data}
+        duration = time.time() - before
+        result['duration'] = duration
+        return json.dumps(result)
+    return wrapper
+
+
+@app.route('/api/<user>/recent')
+@app_api
+def api_recent(user):
+    limit = request.args.get('limit', 1)
+    return lastfm.get_recent_tracks(user, limit)
 
 
 if __name__ == '__main__':
---
+++ lastfm.py
@@ -0,0 +1,30 @@
+import requests
+from pyquery import PyQuery as pq
+
+
+API_ROOT = 'http://ws.audioscrobbler.com/2.0/'
+API_KEY = '4b07bbcee2b8fc6f56a338ad24cbdedf'
+
+
+def last_query(method=None, **kwargs):
+    params = dict(**kwargs)
+    params['method'] = method
+    params['api_key'] = API_KEY
+    return requests.get(API_ROOT, params=params)
+
+
+def parse_track(element):
+    track = pq(element)
+    return {
+        'name': track('name').text(),
+        'artist': track('artist').text(),
+        'time': track('date').attr['uts']
+    }
+
+
+def get_recent_tracks(user, limit=1):
+    resp = last_query('user.getrecenttracks', user=user, limit=limit)
+    if resp.status_code == 200:
+        doc = pq(resp.content)
+        return [parse_track(ele) for ele in doc('track')]
+    return None
